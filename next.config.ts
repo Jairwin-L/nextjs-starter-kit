b50b0d454d4f310e1d01ca15baa8b236dc9f88f4
Jairwin <jairwin@jairwin.cn>
2025-09-05 09:50:33 +0800

    chore: update npm info

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	turbopack: {
+		// https://nextjs.org/docs/app/api-reference/config/next-config-js/turbopack#root-directory
+    root: path.join(__dirname, '..'),
+  },
 };
 
 export default nextConfig;
